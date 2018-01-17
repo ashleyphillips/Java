50021b930b1bbc016e5f2b2d6092781217725efe
ashleyphillips <ashleylphil@gmail.com>
2018-01-17 16:57:10 -0600

    resume update

---
+++ index.js
@@ -0,0 +1,48 @@
+var str = "ashley phillips";
+var res = str.toUpperCase();
+
+console.log('Name: ' + res);
+
+console.log('Career: Web Development');
+
+console.log('Description: Crazy person');
+
+console.log('My Interests:');
+
+console.log('* Tap dancing');
+
+console.log('* Costume making');
+
+console.log('* Cats');
+
+console.log('* Reading');
+
+console.log('* Marketing');
+
+console.log('My Previous Experience:');
+
+console.log('Visitor Services Rep at McWane Science Center');
+
+console.log('Coordinate field trips, facilitate large groups, and ensure gusts have a great time');
+
+console.log('Assistant Manager at Cold Stone Creamery');
+
+console.log('Decorate cakes, train staff members, make ice cream');
+
+console.log('My Skills:');
+
+console.log('* Check It: Tap dancing');
+
+console.log('* Check It: Fluent in 4 languages');
+
+console.log('* Clarinet');
+
+console.log('* Professional Dodgeball');
+
+console.log('* Check It: Dog Whisperer');
+
+console.log('* HTML');
+
+console.log('* CSS');
+
+console.log('* Check It: App Development');
