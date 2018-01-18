f85fa3f3af7573f56dc39cf351cfb2e9d6322ac3
ashleyphillips <ashleylphil@gmail.com>
2018-01-18 13:10:13 -0600

    final

--- index.js
+++ index.js
@@ -22,36 +22,29 @@ console.log('* Marketing');
 console.log('My Previous Experience:');
 
 let displayPosition = function(title, company, description) {
-    console.log(`* ${title} at ${company}`);
-    console.log(description);
+    console.log(`* ${title} at ${company}
+- ${description}`
+    );
 }
 
-displayPosition('Visitor Services Rep', 'McWane Science Center', '- Coordinate field trips, facilitate large groups, and ensure gusts have a great time')
-
-displayPosition('Assistant Manager', 'Cold Stone Creamery', '- Decorate cakes, train staff members, make ice cream')
+displayPosition('Visitor Services Rep', 'McWane Science Center', 'Coordinate field trips, facilitate large groups, and ensure gusts have a great time')
+displayPosition('Assistant Manager', 'Cold Stone Creamery', 'Decorate cakes, train staff members, make ice cream')
 
 console.log('My Skills:');
 
 let displaySkill = function(skill, iscool) {
     if (iscool === true) {
-        console.log(`Check It: ${skill}`);
+        console.log(`* Check It: ${skill}`);
     } else {
-        console.log(skill);
+        console.log(`* ${skill}`);
     }
 }
 
 displaySkill('Tap dancing', true)
-
 displaySkill('Fluent in four languages', true)
-
 displaySkill('Clarinet', false)
-
 displaySkill('Professional dodgeball', false)
-
 displaySkill('Cat whisperer', true)
-
 displaySkill('HTML', false)
-
 displaySkill('CSS', false)
-
 displaySkill('App development', true)
